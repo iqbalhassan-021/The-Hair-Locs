59eafd49d5a12757d44c8d00455749da6d320986
Hassan Ashfaq <102966946+iqbalhassan-021@users.noreply.github.com>
2026-02-03 09:54:57 +0500

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4HVgg92nNrk0nM2sneT0ojFb0HFmOVT4",
-  authDomain: "thehairlocs-94374.firebaseapp.com",
-  projectId: "thehairlocs-94374",
-  storageBucket: "thehairlocs-94374.firebasestorage.app",
-  messagingSenderId: "358707840743",
-  appId: "1:358707840743:web:fe0a61952debef020c8442",
-  measurementId: "G-23H15FBP4S"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
